8ef5fef1bf97e144c4de758825b6568053899ffa
Sarah Wimark <sarahwimark@gmail.com>
2019-06-18 17:27:17 +0200

    Adding modals to display, add and edit techs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Fragment, useEffect } from 'react'
-import 'materialize-css/dist/css/materialize.min.css'
-import M from 'materialize-css/dist/js/materialize.min.js'
-import './App.css'
 import Searchbar from './components/layout/SearchBar'
 import Logs from './components/logs/Logs'
+import AddBtn from './components/layout/AddBtn'
+import AddLogModal from './components/logs/AddLogModal'
+import EditLogModal from './components/logs/EditLogModal'
+import AddTechModal from './components/techs/AddTechModal'
+import TechlistModal from './components/techs/TechListModal'
+import './App.css'
+import 'materialize-css/dist/css/materialize.min.css'
+import M from 'materialize-css/dist/js/materialize.min.js'
 
 const App = () => {
   useEffect(() => {
@@ -15,6 +20,11 @@ const App = () => {
     <Fragment>
       <Searchbar />
       <div className='container'>
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechlistModal />
         <Logs />
       </div>
     </Fragment>
